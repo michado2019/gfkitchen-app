a36f81fe517dc98a6315f0e03e8d5328dc6d3724
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-03-28 06:33:06 +0100

    style: small screen nav style modified

--- src/components/pages/meetOurMd/MeetOurMd.jsx
+++ src/components/pages/meetOurMd/MeetOurMd.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './MeetOurMd.css'
+import mdPix from './assets/profile-pix.jpg'
 const MeetOurMd = () => {
   return (
-    <div className='meetOur-md_wrapper'>MeetOurMd</div>
+    <div className='meetOur-md_wrapper'>
+      <div className='meetOur-md_content'>
+        <div className='meetOur-md_info'>
+          <img src={mdPix} alt='img' className='meetOur-md_pix'/>
+        </div>
+        <div className='meetOur-md_speech'>
+          <h2 className='meetOur-md_speechTitle'>Hello, welcome to GFkitchen</h2>
+          <p className='meetOur-md_speechDetails'>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+          <h2 className='meetOur-md_name'>--- Sam God's favour.</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
