ce44dd1f8d1ef7e1987cddfe74f6d7ca396a3dbc
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-04-01 02:45:27 +0100

    home done with its media queries

--- src/components/pages/home/AboutUS.jsx
+++ src/components/pages/home/AboutUS.jsx
@@ -1,30 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ourKitchenData from "../ourKitchen/ourKitchenData/OurKitchenData";
 import "./AboutUs.css";
 const AboutUS = () => {
-    
+  const [allData] = React.useState(ourKitchenData);
+  const [change, setChange] = React.useState(1);
+  const [setSlide] = React.useState(false);
+  const [randomData, setRandomData] = React.useState({
+    ditchImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1XgTReRh2chXGLJXjhdqTbpUuVjxaPwI3Cw&usqp=CAU",
+    ditchName: 'GFkitchen',
+    price: 'Affordable'
+
+  });
+
+  function getNewData() {
+    const allDataArray = allData;
+    const randomData = Math.floor(Math.random() * allDataArray.length);
+    const kitchenData = allDataArray[randomData];
+    setRandomData(kitchenData)
+  }
+
+  setInterval(() => {
+     if(change < allData.length){
+      getNewData()
+     }
+     setSlide(prev => !prev)
+  }, 3000)
+
+  useEffect(() => {
+    setChange(allData.length)
+  }, [allData.length])
   return (
     <div className="aboutUs-wrapper" id="about">
       <div className="aboutUs-contents">
-      <div className="aboutUs-content1">
-        <h2 className="aboutUs-title">About</h2>
-        <p className="aboutUs-details">
-          Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum, Lorem ipsum lorem
-          ipsum. Lorem ipsum lorem ipsum, Lorem ipsum lorem ipsum, Lorem ipsum.
-          lorem ipsum Lorem ipsum lorem ipsum, Lorem ipsum lorem ipsum, Lorem.
-          ipsum. lorem ipsum Lorem ipsum lorem ipsum, Lorem ipsum lorem ipsum.
-          lorem ipsum Lorem ipsum.lorem ipsum
-        </p>
+        <div className="aboutUs-content1">
+          <h2 className="aboutUs-title">About</h2>
+          <p className="aboutUs-details">
+            Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum, Lorem ipsum lorem
+            ipsum. Lorem ipsum lorem ipsum, Lorem ipsum lorem ipsum, Lorem
+          </p>
         </div>
         <div className="aboutUs-content2">
-             <div className="aboutUs-content2-img">
-
-             </div>
-             <div className="aboutUs-content2-name">
-              
-             </div>
-             <div className="aboutUs-content2-value">
-              
-             </div>
+          <div className="aboutUs-content2_content">
+            <img src={randomData.ditchImg} alt='img' className="aboutUs-content2_img"/>
+            <h2 className="aboutUs-content2_name">{randomData.ditchName}</h2>
+            <h3 className="aboutUs-content2_price">{randomData.price}</h3>
+          </div>
         </div>
       </div>
     </div>
