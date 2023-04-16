1bc517bd4ab7231c69a7f7eff67f477b409ed467
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-04-16 21:07:47 +0100

    feat: navigating to login when there is no user at admin

--- src/components/pages/admin/Admin.jsx
+++ src/components/pages/admin/Admin.jsx
@@ -24,9 +24,6 @@ const Admin = () => {
   //Localstorage
   const form = localStorage.getItem("form");
   const user = JSON.parse(form);
-  if (!form) {
-    navigate("/login");
-  }
   // Getting data from firebase
   onSnapshot(dbRef, (snapshot) => {
     const dishData = snapshot.docs.map((doc) => ({
@@ -49,70 +46,76 @@ const Admin = () => {
   };
   return (
     <div className="adminWrapper">
-      <h2 className="adminHeading">Wecome, {user.firstName}.</h2>
-      <div className="adminContents">
-        <div className="adminContent">
-          <div className="adminContents-div">
-            <div className="adminDetails-div">
-              <h2 className="adminDetail-heading">Admin details:</h2>
-              <h3 className="adminDetail-name">
-                Name: {user.firstName} {user.lastName}
-              </h3>
-              <h3 className="adminDetail-email">Email: {user.email}</h3>
-            </div>
-            <div className="adminDetails-content2">
-              <h2 className="adminDetails-content2_heading">
-                Admin, don't forget our core value:
-              </h2>
-              <p className="adminDetails-content2_details">
-                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                Lorem ipsum Lorem ipsum Lorem ipsum
-              </p>
-            </div>
-          </div>
-          <div className="adminOrdersDetails">
-            <h2 className="adminDish-heading">Order details:</h2>
-            {dishData.slice(skip, skip + perPage).map((data) => {
-              return (
-                <div className="adminDish" key={data.id}>
-                  <div className="adminDish-div">
-                    <div className="adminDish-div1">
-                      <img
-                        src={data.dishImg}
-                        alt="img"
-                        className="adminDish-img"
-                      />
-                      <h2 className="adminDish-name">{data.dishName}</h2>
-                      <h3 className="adminDish-price">{data.price}</h3>
-                    </div>
-                    <div className="adminDish-div2">
-                      <h3 className="adminDish-plates">
-                        Plates: {data.plates}
-                      </h3>
-                      <h3 className="adminDish-totalPrice">
-                        Total Price: #{data.totalPrice}
-                      </h3>
-                      <h3 className="adminDish-name">Name: {data.name}</h3>
-                      <h3 className="adminDish-time">Time: {data.time}</h3>
-                      <button
-                        className="adminDish-btn"
-                        onClick={() => handleDelete(data.id)}
-                      >
-                        Complete order
-                      </button>
-                    </div>
-                  </div>
+      {!user ? (
+        navigate("/login")
+      ) : (
+        <div>
+          <h2 className="adminHeading">Wecome, {user.firstName}.</h2>
+          <div className="adminContents">
+            <div className="adminContent">
+              <div className="adminContents-div">
+                <div className="adminDetails-div">
+                  <h2 className="adminDetail-heading">Admin details:</h2>
+                  <h3 className="adminDetail-name">
+                    Name: {user.firstName} {user.lastName}
+                  </h3>
+                  <h3 className="adminDetail-email">Email: {user.email}</h3>
                 </div>
-              );
-            })}
-            <div className="adminDish-pagination">
-              <Pagination page={page} pages={pages} setPage={setPage} />
+                <div className="adminDetails-content2">
+                  <h2 className="adminDetails-content2_heading">
+                    Admin, don't forget our core value:
+                  </h2>
+                  <p className="adminDetails-content2_details">
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                    Lorem ipsum Lorem ipsum Lorem ipsum
+                  </p>
+                </div>
+              </div>
+              <div className="adminOrdersDetails">
+                <h2 className="adminDish-heading">Order details:</h2>
+                {dishData.slice(skip, skip + perPage).map((data) => {
+                  return (
+                    <div className="adminDish" key={data.id}>
+                      <div className="adminDish-div">
+                        <div className="adminDish-div1">
+                          <img
+                            src={data.dishImg}
+                            alt="img"
+                            className="adminDish-img"
+                          />
+                          <h2 className="adminDish-name">{data.dishName}</h2>
+                          <h3 className="adminDish-price">{data.price}</h3>
+                        </div>
+                        <div className="adminDish-div2">
+                          <h3 className="adminDish-plates">
+                            Plates: {data.plates}
+                          </h3>
+                          <h3 className="adminDish-totalPrice">
+                            Total Price: #{data.totalPrice}
+                          </h3>
+                          <h3 className="adminDish-name">Name: {data.name}</h3>
+                          <h3 className="adminDish-time">Time: {data.time}</h3>
+                          <button
+                            className="adminDish-btn"
+                            onClick={() => handleDelete(data.id)}
+                          >
+                            Complete order
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+                <div className="adminDish-pagination">
+                  <Pagination page={page} pages={pages} setPage={setPage} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
