ca713f06e49ae16043a22d48d0623e9b9b4e6097
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-04-30 03:31:04 +0100

    feat: error boundaryy added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,32 @@ import SmallScreenNav from "./components/SmallScreenNav";
 import ourKitchenData from "./components/pages/ourKitchen/ourKitchenData/OurKitchenData";
 import { db } from "./components/firebase";
 import { getDocs, collection } from "firebase/firestore";
+import { ErrorBoundary } from "react-error-boundary";
+import { useNavigate } from "react-router-dom";
 export const DbContext = createContext();
 
 function App() {
+  // Navigation
+  const navigate = useNavigate();
+
+  //Reload
+  const handleReload = () => {
+    navigate("/");
+    window.location.reload();
+  };
+
+  // ErrorBoundary
+  const ErrorBoundaryComponent = ({ error }) => {
+    return (
+      <div role="alert" className="errorBoundary">
+        <h1 className="errorBoundary-title">Something went wrong!</h1>
+        <p className="errorBoundary-message">{error.message}</p>
+        <button className="errorBoundary-btn" onClick={handleReload}>
+          Go back home
+        </button>
+      </div>
+    );
+  };
   //States
   const [storage, setStorage] = useState([]);
   const [display, setDisplay] = useState(false);
@@ -41,35 +64,37 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <DbContext.Provider value={db}>
-        <Header
-          display={display}
-          setDisplay={setDisplay}
-          cartDisplay={cartDisplay}
-          setCartDisplay={setCartDisplay}
-          input={input}
-          ourKitchenDisplay={ourKitchenDisplay}
-        />
-        <SmallScreenNav display={display} setDisplay={setDisplay} />
-        <div className="appCart-away"></div>
-        <div className="sidebarRouter">
-          <Sidebar ourKitchenDisplay={ourKitchenDisplay} />
-          <AppRouter
-            ourKitchenDishes={ourKitchenData}
-            storage={storage}
-            setStorage={setStorage}
+      <ErrorBoundary FallbackComponent={ErrorBoundaryComponent}>
+        <DbContext.Provider value={db}>
+          <Header
+            display={display}
+            setDisplay={setDisplay}
             cartDisplay={cartDisplay}
             setCartDisplay={setCartDisplay}
             input={input}
-            setInput={setInput}
-            docsLength={docsLength}
-            loading={loading}
-            setLoading={setLoading}
-            setOurKitchenDisplay={setOurKitchenDisplay}
+            ourKitchenDisplay={ourKitchenDisplay}
           />
-        </div>
-        <Footer />
-      </DbContext.Provider>
+          <SmallScreenNav display={display} setDisplay={setDisplay} />
+          <div className="appCart-away"></div>
+          <div className="sidebarRouter">
+            <Sidebar ourKitchenDisplay={ourKitchenDisplay} />
+            <AppRouter
+              ourKitchenDishes={ourKitchenData}
+              storage={storage}
+              setStorage={setStorage}
+              cartDisplay={cartDisplay}
+              setCartDisplay={setCartDisplay}
+              input={input}
+              setInput={setInput}
+              docsLength={docsLength}
+              loading={loading}
+              setLoading={setLoading}
+              setOurKitchenDisplay={setOurKitchenDisplay}
+            />
+          </div>
+          <Footer />
+        </DbContext.Provider>
+      </ErrorBoundary>
     </div>
   );
 }
